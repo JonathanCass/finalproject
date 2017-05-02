844a854c7b6c07715e997b4c01a7c779c05ea9c2
Josephine Gabrielle Baltazar <jgsbaltazar@yahoo.com>
2017-05-02 12:29:36 -0700

    carousel working

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,24 +1,57 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import '../assets/home.css'
+import Carousel from 'nuka-carousel'
+// import {Link} from 'react-router-dom'
 
-const styles={
-    link:{
-        margin: 10
-    }
-}
+
 class Home extends React.Component {
 //   constructor(props) {
  
 //    super(props)
 //   }
 
+handleSubmit = (e) => {  // FINISH HANDLE FUNCTIONS
+  e.preventDefault()
+
+}
+handleClick = (e) => {
+  e.preventDefault()
+  console.log('click')
+}
+handleChange = (e) => {
+  this.setState({
+    [e.target.name]:e.target.value
+    
+  })
+}
+
   render() {
     return (
-      <div>
-        Home Page
-        <Link to={'/CreateUser/'} style={styles.link}>Create User</Link>
-        <Link to={'/User/'} style={styles.link}>User</Link>
-        <Link to={'/ParkView/'} style={styles.link}>ParkView</Link>
+      <div className="beginningContainer">
+        <img alt='' id="logo" src={require('../assets/Logo.png')}/>
+        <form onSubmit={this.handleSubmit} className="user">
+          <button onClick={this.handleClick}>Sign Up</button>
+          <input type='text' onChange={this.handleChange} name='login' placeholder='Login' />
+        </form>
+        <h4>Social Media Icons</h4>
+        <div className="topLine"></div>
+        <section className="carouselContainer">
+          <Carousel>
+            {/*<div className="pictures"></div>*/}
+              <img alt='' src="https://images.pexels.com/photos/798/bench-people-smartphone-sun.jpg?w=940&amp;h=650&amp;auto=compress&amp;cs=tinysrgb"/>
+              <img alt='' src="https://static.pexels.com/photos/24306/pexels-photo-24306.jpg"/>
+              <img alt='' src="https://static.pexels.com/photos/386024/pexels-photo-386024.jpeg"/>
+              <img alt='' src="https://static.pexels.com/photos/305244/pexels-photo-305244.jpeg"/>
+              <img alt='' src="http://latina.lu/wp-content/uploads/2017/04/tenis-003.jpg"/>
+          </Carousel>  
+       </section>
+       <h1 className="happening">What's Happening In Your Area</h1>
+          <ul className="matches">
+            <li id="person">Match</li>
+            <li id="person">Match</li>
+            <li id="person">Match</li>
+            <li id="person">Match</li>
+          </ul>
       </div>
     )
   }
