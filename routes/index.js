7faa6aa552ab85f8b47c6da3a36d5456fc3933d4
Martinez Aytch <2kproductionss@gmail.com>
2017-05-05 19:45:03 -0700

    able to get and post to database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+// const sha1 = require('sha1')
 
 /* GET home page. */
 
@@ -7,7 +8,7 @@ var router = express.Router();
 //host: 'http://10.68.0.58',
 const mysql = require('mysql')
 const conn = mysql.createConnection({
-//   host     : 'http://10.68.0.58',
+  host     : 'http://10.68.0.58',
   host     : 'localhost',
   user     : 'project',
   password : '-?AT6.GFaj`z%Uja',
@@ -37,7 +38,7 @@ router.get('/event', function(req, res){
 
 router.get('/parks', function(req, res){
    conn.query('SELECT * FROM parks', function(err, results){
-       console.log(results)
+      //  console.log(results)
        var response = {
            parks: results
        }
@@ -48,7 +49,7 @@ router.get('/parks', function(req, res){
 
 router.get('/parks_activities', function(req, res){
    conn.query('SELECT * FROM parks_activities', function(err, results){
-       console.log(results)
+      //  console.log(results)
        var response = {
            parks_activities: results
        }
@@ -74,5 +75,85 @@ router.get('/users_activities', function(req, res){
         res.json(response)
     })
 })
+
+router.get('/event', function(req, res){
+    conn.query('SELECT * FROM event', function(err, results){
+        var response = {
+            event: results
+        }
+        res.json(response)
+    })
+})
+
+router.get('/availability', function(req, res){
+  console.log('availability', res)
+    conn.query('SELECT * FROM availability', function(err, results){
+        var response = {
+            availability: results
+        }
+        res.json(response)
+    })
+})
+
+
+///using /activities as a test
+
+// router.post('/activities', function(req, res){
+//   var sql = 'INSERT INTO activities (id, name) VALUES (?, ?)'
+
+//   conn.query(sql, [req.body.id, req.body.name], function(err, results){
+//         res.json({
+//           'message':'Activity Added'
+//         })
+//   })
+// })
+
+router.post('/users', function(req, res){
+  console.log('client post', res.body)
+  var sql = 'INSERT INTO users (id, fname, lname, email, avatar, bio, privacy, activities_info, hobbies, interests, age, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+
+  conn.query(sql, [req.body.id, req.body.fname, req.body.lname, req.body.email, req.body.avatar, req.body.bio, req.body.privacy, req.body.activities_info, req.body.hobbies, req.body.interests,req.body.age,req.body.gender], function(err, results){
+    if(err){
+      res.json({
+        'message':'Error... User Not Added'
+      })
+    }
+     res.json({
+       'message':'User Added'
+     })
+  })
+})
+
+router.post('/event', function(req, res){
+  var sql = 'INSERT INTO event (id, user_id1, user_id2, activity_id, park_id, time_start, time_stop) VALUES (?, ?, ?, ?, ?, ?)'
+
+  conn.query(sql, [req.body.id, req.body.user_id1, req.body.user_id2, req.body.activity_id, req.body.park_id, req.body.time_start], function(err, results){
+    if(err){
+      res.json({
+         'message':'Error... Event Not Added'
+      })
+    } else 
+    res.json({
+      'message': 'Event Added'
+    })
+  })
+})
+
+router.post('/availability', function(req, res){
+  var sql = 'INSERT INTO availability (id, user_id, quadrant, day_of_week, from_num, from_suffix, to_num, to_suffix) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+
+  conn.query(sql, [req.body.id, req.body.user_id, req.body.quadrant, req.body.day_of_week, req.body.from_num, req.body.from_suffix, req.body.to_num, req.body.to_suffix], function(err, results){
+    if(err){
+      res.json({
+        'message': 'Error... Availability Not Added'
+      })
+    } else
+    res.json({
+      'message': 'Added Availability'
+    })
+  })
+})
+
+router.post
 
 module.exports = router;
