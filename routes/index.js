0ea20afbb9a13bec6e90025d0491dc48b21d68a3
Martinez Aytch <2kproductionss@gmail.com>
2017-05-10 13:51:25 -0700

    saving changes made

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 
 //CONNECT TO DATABASE 
-//host: 'http://10.68.0.58',
+// host: 'http://10.68.0.58',
 const mysql = require('mysql')
 const conn = mysql.createConnection({
   host     : 'http://10.68.0.58',
@@ -38,7 +38,6 @@ router.get('/event', function(req, res){
 
 router.get('/parks', function(req, res){
    conn.query('SELECT * FROM parks', function(err, results){
-      //  console.log(results)
        var response = {
            parks: results
        }
@@ -62,6 +61,15 @@ router.get('/users', function(req, res){
     conn.query('SELECT * FROM users', function(err, results){
         var response = {
             users: results
+        }
+        res.json(response)
+    })
+})
+
+router.get('/usersId', function(req, res){
+    conn.query('SELECT id FROM users order by id desc limit 1;', function(err, results){
+        var response = {
+            usersId: results
         }
         res.json(response)
     })
@@ -110,9 +118,9 @@ router.get('/availability', function(req, res){
 
 router.post('/users', function(req, res){
   console.log('client post', res.body)
-  var sql = 'INSERT INTO users (id, fname, lname, email, avatar, bio, privacy, activities_info, hobbies, interests, age, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+  var sql = 'INSERT INTO users (fname, lname, email, avatar, bio, privacy, activities_info, hobbies, interests, age, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
 
-  conn.query(sql, [req.body.id, req.body.fname, req.body.lname, req.body.email, req.body.avatar, req.body.bio, req.body.privacy, req.body.activities_info, req.body.hobbies, req.body.interests,req.body.age,req.body.gender], function(err, results){
+  conn.query(sql, [req.body.fname, req.body.lname, req.body.email, req.body.avatar, req.body.bio, req.body.privacy, req.body.activities_info, req.body.hobbies, req.body.interests,req.body.age,req.body.gender], function(err, results){
     if(err){
       res.json({
         'message':'Error... User Not Added'
@@ -125,9 +133,9 @@ router.post('/users', function(req, res){
 })
 
 router.post('/event', function(req, res){
-  var sql = 'INSERT INTO event (id, user_id1, user_id2, activity_id, park_id, time_start, time_stop) VALUES (?, ?, ?, ?, ?, ?)'
+  var sql = 'INSERT INTO event (user_id1, user_id2, activity_id, park_id, time_start_hour, time_start_suffix, date_day, date_month) VALUES (?,?,?,?, ?, ?, ?, ?)'
 
-  conn.query(sql, [req.body.id, req.body.user_id1, req.body.user_id2, req.body.activity_id, req.body.park_id, req.body.time_start], function(err, results){
+  conn.query(sql, [req.body.user_id1, req.body.user_id2, req.body.activity_id, req.body.park_id, req.body.time_start_hour, req.body.time_start_suffix, req.body.date_day, req.body.date_month], function(err, results){
     if(err){
       res.json({
          'message':'Error... Event Not Added'
@@ -139,10 +147,12 @@ router.post('/event', function(req, res){
   })
 })
 
-router.post('/availability', function(req, res){
-  var sql = 'INSERT INTO availability (id, user_id, quadrant, day_of_week, from_num, from_suffix, to_num, to_suffix) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
 
-  conn.query(sql, [req.body.id, req.body.user_id, req.body.quadrant, req.body.day_of_week, req.body.from_num, req.body.from_suffix, req.body.to_num, req.body.to_suffix], function(err, results){
+
+router.post('/availability', function(req, res){
+  var sql = 'INSERT INTO availability ( user_id, quadrant, day_of_week, from_num, from_suffix, to_num, to_suffix) VALUES (?, ?, ?, ?, ?, ?, ?)'
+
+  conn.query(sql, [req.body.user_id, req.body.quadrant, req.body.day_of_week, req.body.from_num, req.body.from_suffix, req.body.to_num, req.body.to_suffix], function(err, results){
     if(err){
       res.json({
         'message': 'Error... Availability Not Added'
